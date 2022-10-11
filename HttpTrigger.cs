581e9e53462836abf72c9c15e39b53ddbe286f34
Héctor Rodríguez Parada <55230156+Hector-commits@users.noreply.github.com>
2022-10-11 10:33:32 +0200

    Initial commit

---
+++ HttpTrigger.cs
@@ -0,0 +1,76 @@
+#r "Newtonsoft.Json"
+#r "C:\home\site\wwwroot\HttpTrigger\bin\itextsharp.dll"
+#r "C:\home\site\wwwroot\HttpTrigger\bin\SigPDF.dll"
+
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+
+public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
+{
+    log.LogInformation("C# HTTP trigger function processed a request.");
+
+    string pdfToSign = req.Query["pdfToSign"];
+    string certificate = req.Query["certificate"];
+    
+    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+    dynamic data = JsonConvert.DeserializeObject(requestBody);
+    pdfToSign = pdfToSign ?? data?.pdfToSign;
+    certificate = certificate ?? data?.certificate;
+        
+    string responseMessage = string.IsNullOrEmpty(pdfToSign)
+        ? "This HTTP triggered function executed successfully. Pass a pdfToSign to stamp the certificate!"
+                : $"Hello, {pdfToSign}. This HTTP triggered function executed successfully and we stamp the {certificate}.";
+
+            return new OkObjectResult(responseMessage);
+}
+
+// POST api/values
+public async Task<object> Post()
+{
+    if (!Request.Content.IsMimeMultipartContent("form-data"))
+    {
+        throw new HttpResponseException
+            (Request.CreateResponse(HttpStatusCode.UnsupportedMediaType));
+    }
+    MultipartFormDataStreamProvider streamProvider = new MultipartFormDataStreamProvider(
+        HttpContext.Current.Server.MapPath("~/Images/"));
+    await Request.Content.ReadAsMultipartAsync(streamProvider);
+
+
+    foreach (MultipartFileData fileData in streamProvider.FileData)
+    {
+        string fileName = "";
+        if (string.IsNullOrEmpty(fileData.Headers.ContentDisposition.FileName))
+        {
+            fileName = Guid.NewGuid().ToString();
+        }
+        fileName = fileData.Headers.ContentDisposition.FileName;
+        if (fileName.StartsWith("\"") && fileName.EndsWith("\""))
+        {
+            fileName = fileName.Trim('"');
+        }
+        if (fileName.Contains(@"/") || fileName.Contains(@"\"))
+        {
+            fileName = Path.GetFileName(fileName);
+        }                
+
+        File.Move(fileData.LocalFileName,
+            Path.Combine(HttpContext.Current.Server.MapPath("~/Images/"), fileName));
+    }
+
+    
+    SigPDF.FirmaPDF Firma = new SigPDF.FirmaPDF();
+    Firma.Sign(HttpContext.Current.Server.MapPath("~/Images/alazaro_1234.pfx"), "1234", HttpContext.Current.Server.MapPath("~/Images/doc.pdf"),
+        HttpContext.Current.Server.MapPath("~/Images/docSign.pdf"), "", "", "", true);
+
+
+    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+    FileStream fileStream = File.OpenRead(HttpContext.Current.Server.MapPath("~/Images/docSign.pdf"));
+    response.Content = new StreamContent(fileStream);
+    response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+
+    return response;
+    
+}
